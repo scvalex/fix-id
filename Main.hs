26ac0e7224e76255bf078e56b934ab37ff9cb56d
Alexandru Scvortov <scvalex@gmail.com>
2011-11-26 00:24:34 +0000

    add basic webserver

--- Main.hs
+++ Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import Data.Monoid ( Monoid(..) )
+import Blaze.ByteString.Builder.Char.Utf8 ( fromText )
+import Network.HTTP.Types ( statusOK )
+import Network.Wai ( Application, Request(..), Response(..) )
+import Network.Wai.Handler.Warp ( run )
+
 main :: IO ()
-main = putStrLn "Ok"
+main = run 50080 fixId
+
+fixId :: Application
+fixId req = let path = pathInfo req
+            in return (ResponseBuilder statusOK [] (fromText $ mconcat path))
