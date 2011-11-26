4d9e7cf6ee44c5acd24b3266b6072056b9208349
Alexandru Scvortov <scvalex@gmail.com>
2011-11-26 20:51:37 +0000

    html for posts

--- Handler/Post.hs
+++ Handler/Post.hs
@@ -3,16 +3,38 @@
 -- | The 'Post' handler serves the content of a post, or a form for
 -- creating/updating a post.
 module Handler.Post (
-        post
+        post, postFragment
     ) where
 
-import Blaze.ByteString.Builder.Char.Utf8 ( fromText )
+import Data.Text ( Text )
 import Logger ( noticeM )
 import Network.HTTP.Types ( statusOK )
 import Network.Wai ( Application, Request(..), Response(..) )
+import Text.Blaze ( Html, ToHtml(..) )
+import Text.Blaze.Html5 ( (!) )
+import qualified Text.Blaze.Html5 as H
+import qualified Text.Blaze.Html5.Attributes as A
+import Text.Blaze.Renderer.Utf8 ( renderHtmlBuilder )
 import Text.Interpol ( (^-^) )
 
 post :: Application
 post req = do
   noticeM $ "Handling post " ^-^ (rawPathInfo req)
-  return (ResponseBuilder statusOK [] (fromText "Viewing a post"))
+  return (ResponseBuilder statusOK [] $ renderHtmlBuilder postPage)
+
+postPage :: Html
+postPage = do
+  H.docType
+  H.html $ do
+    H.head $ do
+             H.title "New Post"
+    H.body $ do
+             postFragment
+
+postFragment :: Html
+postFragment = do
+  H.form ! A.action "/post" ! A.method "POST" $ do
+    H.div $
+     H.textarea ! A.rows "20" ! A.cols "80" $
+      toHtml ("Insightful comment" :: Text)
+    H.button (toHtml ("Submit" :: Text))
